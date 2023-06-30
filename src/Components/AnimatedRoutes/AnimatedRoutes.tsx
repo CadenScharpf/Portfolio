892af24a9c593e606e99b5d4c55f933a48e83e17
Caden Scharpf <u16scharpf@gmail.com>
2023-06-29 21:36:41 -0700

    add nav items to appbar

--- src/Components/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/Components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -1,38 +1,35 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { Home, PAGES, Resume, Works } from '../../Pages';
-import { AnimatePresence, motion, PanInfo, Variant, Variants } from 'framer-motion';
-import { WebApplications } from '../../Pages/Works/Components';
-import { BackendApps } from '../../Pages/Works/Components/BackendApps';
-import { TestPage } from '../../Pages/TestPage/TestPage';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { ContentWindow } from '../ContentWindow';
-import { Box, Container, Typography } from '@mui/material';
-import { wrap } from '@popmotion/popcorn';
-import { Nav } from '../Nav';
-import { LayoutContext } from '../../Context';
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Home, PAGES, Resume, Works } from "../../Pages";
+import {
+  AnimatePresence,
+  motion,
+  PanInfo,
+  Variant,
+  Variants,
+} from "framer-motion";
+import { WebApplications } from "../../Pages/Works/Components";
+import { BackendApps } from "../../Pages/Works/Components/BackendApps";
+import { TestPage } from "../../Pages/TestPage/TestPage";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { ContentWindow } from "../ContentWindow";
+import { Box, Container, Typography } from "@mui/material";
+import { wrap } from "@popmotion/popcorn";
+import { Nav } from "../Nav";
+import { LayoutContext } from "../../Context";
 
-const GRADIENT_COLOR_1 = 'blue'
-const GRADIENT_COLOR_2 = 'red'
-const BORDER_COLOR = `linear-gradient(white, white) padding-box, linear-gradient(to right, ${GRADIENT_COLOR_1}, ${GRADIENT_COLOR_2}) border-box`
-
-
-
-const navVariants: Variants = {
-  initial: { width: 0 },
-  animate: { width: 20, boxShadow: '0 0 5px black', transition: { delay: .5, type: 'spring', bounce: .4} },
-  exit: { width: 0 },
-  hover: { width: 30, boxShadow: '0 0 7px 0px black',  transition: {  type: 'spring', bounce: .5}, },
-}
+const GRADIENT_COLOR_1 = "blue";
+const GRADIENT_COLOR_2 = "red";
+const BORDER_COLOR = `linear-gradient(white, white) padding-box, linear-gradient(to right, ${GRADIENT_COLOR_1}, ${GRADIENT_COLOR_2}) border-box`;
 
 const sliderVariants: Variants = {
   incoming: (direction: number) => ({
-    x: direction > 0 ? '100%' : '-100%',
+    x: direction > 0 ? "100%" : "-100%",
     scale: 1.2,
     opacity: 0,
   }),
   active: { x: 0, scale: 1, opacity: 1 },
   exit: (direction: number) => ({
-    x: direction > 0 ? '-100%' : '100%',
+    x: direction > 0 ? "-100%" : "100%",
     scale: 1,
     opacity: 0,
   }),
@@ -44,7 +41,9 @@ const sliderTransition = {
 };
 
 export function AnimatedRoutes() {
-  const [[itemCount, direction], setItemCount] = useState<[number, number]>([0, 1]);
+  const [[itemCount, direction], setItemCount] = useState<[number, number]>([
+    0, 1,
+  ]);
   const activeItemIndex = wrap(0, PAGES.length, itemCount);
 
   const swipeToItem = (swipeDirection: number) => {
@@ -55,19 +54,19 @@ export function AnimatedRoutes() {
     const draggedDistance = dragInfo.offset.x;
     const swipeThreshold = 50;
     if (draggedDistance > swipeThreshold) {
-      swipeToItem(-1);
+      swipeToItem(1 /* -1 */);
     } else if (draggedDistance < -swipeThreshold) {
       swipeToItem(1);
     }
   };
 
   const skipToItem = (imageId: number) => {
-    setItemCount([imageId, imageId > activeItemIndex ? 1 : -1]);
+    setItemCount([imageId, imageId > activeItemIndex ? 1 : 1/* -1 */]);
   };
 
   return (
     <AnimatePresence initial={true}>
-      <Nav/>
+      <Nav setPage={skipToItem} />
       <Box
         component={motion.div}
         key={"page" + activeItemIndex}
@@ -78,40 +77,37 @@ export function AnimatedRoutes() {
         exit="exit"
         transition={sliderTransition}
         drag="x"
-        dragConstraints={{ left: 0, right: 0 }}
         onDragEnd={(_, dragInfo) => dragEndHandler(dragInfo)}
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'row',
-          padding: '1rem',
-          alignItems: 'center',
-          margin: '1rem',
-          position: 'absolute',
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          padding: "1rem",
+          alignItems: "center",
+          margin: "1rem",
+          position: "absolute",
           top: LayoutContext.navHeight,
           bottom: 0,
-          width: '90%',
+          width: "90%",
           px: 0,
-          
         }}
       >
-        
-        <Box 
-        className="content-window"
-        sx={{
-          width: '95%', 
-          height: '100%', 
-          border: '4px solid transparent',
-          borderWidth: '5px',
-          borderRadius: '25px',
-          background: BORDER_COLOR,
-          boxShadow: '0px 0px 5px 0px black',
-          overflow: 'scroll',
-          maxWidth: '1700px'
-        }}>
-        {React.createElement(PAGES[activeItemIndex].component)}
+        <Box
+          className="content-window"
+          sx={{
+            width: "95%",
+            height: "100%",
+            border: "4px solid transparent",
+            borderWidth: "5px",
+            borderRadius: "25px",
+            background: BORDER_COLOR,
+            boxShadow: "0px 0px 5px 0px black",
+            overflow: "scroll",
+            maxWidth: "1700px",
+          }}
+        >
+          {React.createElement(PAGES[activeItemIndex].component)}
         </Box>
-        
       </Box>
     </AnimatePresence>
   );
--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { NavLink } from "react-router-dom";
 import { LayoutContext } from '../../Context'
-import { AppBar, Box, useTheme } from '@mui/material';
+import { PAGES } from '../../Pages'
+import { AppBar, Box, Button, Stack, useTheme } from '@mui/material';
 
 import Logo from './logo.png'
 
-export function Nav() {
+interface iNavProps {
+  setPage: (pageId: number) => void
+}
+
+export function Nav(props: iNavProps) {
   const theme = useTheme();
   
   const styles: Record<string, React.CSSProperties> = {
@@ -36,13 +41,19 @@ export function Nav() {
   
   return (
     <AppBar className='nav' sx={styles.nav}>
-    <Box sx={{width: '50px', position: 'absolute', left: 10}}>
+    <Box sx={{width: '50px', position: 'absolute', left: 10, }}>
       <img alt="" src={Logo} style={{height: '50px', margin: '.5rem'}}/>
       </Box>
     
       
-      <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: { xs: 'center', md: 'right' }, height: '100%'}}>
-        cvxb
+      <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: { xs: 'center', md: 'right' }, height: '100%', color: 'black', alignItems: 'center'}}>
+        <Stack direction="row" sx={{px: 5}}>
+          {PAGES.map((page) => {
+            return (
+              <Button sx={styles.navItem} onClick={() => {props.setPage(page.id)}}>{page.title}</Button>
+            )
+          })}
+        </Stack>
       </Box>
     </AppBar>
   )
